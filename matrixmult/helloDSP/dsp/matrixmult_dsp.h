6835a3189d0ca4aed269b17bb08688a690913d2c
Imran Ashraf <iimran.aashraf@gmail.com>
2017-05-01 16:19:22 +0200

    Added folder matrixmult, which contains helloDSP merged with matrixmult.c

---
+++ matrixmult/helloDSP/dsp/matrixmult_dsp.h
@@ -0,0 +1,13 @@
+#ifndef MATRIXMULT_DSP
+#define MATRIXMULT_DSP
+
+/*TODO Matrix size */
+#define MAX_MATSIZE 128
+
+/*TODO   */
+Uint8 matrixSize;
+
+
+#endif
+
+
---
+++ matrixmult/helloDSP/gpp/matrixmult.h
@@ -0,0 +1,6 @@
+	/*TODO Matrix size */
+#define SIZE 128
+
+int mat1[SIZE][SIZE], mat2[SIZE][SIZE], prod[SIZE][SIZE];
+	
+
