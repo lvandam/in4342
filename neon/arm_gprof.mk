884cfac22c2dce0410fff176c8fc9bcc262bcbcf
Danielle van der Werff <daniellevanderwerff@gmail.com>
2017-06-03 22:26:58 +0200

    Revert "Merge branch 'master' of https://github.com/lvandam/in4342"
    This reverts commit 3f542417a17b293e01c1c604886afe7169a57c65, reversing
    changes made to 37eb7e2fd2482fa39061120f29f8196a2909b967.

--- neon/arm_gprof.mk
+++ neon/arm_gprof.mk
@@ -1,19 +1,20 @@
 BASE_TOOLCHAIN=/opt/bbToolChain/usr/local/share/codesourcery
 CC=$(BASE_TOOLCHAIN)/bin/arm-none-linux-gnueabi-g++
-BEAGLE_DIR=/home/root/esLAB/zdanilo/neon
+MCPROF_DIR=~/mcprof
 
 SRCS=meanshift.cpp main.cpp
 OBJS=$(SRCS:%.cpp=%.o)
 EXEC=armMeanshiftExec
+CMD=./$(EXEC) car.avi
 
-LDFLAGS=-lpthread -lm --sysroot=/opt/rootfs
+LDFLAGS=-lpthread -lm --sysroot=/opt/rootfs -pg
 LIBS=-lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video \
 	-lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_flann
 
 DEFS=-DARMCC
 INCLUDES=-I. -I$(BASE_TOOLCHAIN)/include
 CFLAGS=$(DEFS) $(INCLUDES)          \
-	  -Wall -O3 -Wfatal-errors 		\
+	  -Wall -O2 -Wfatal-errors 		\
 	  --sysroot=/opt/rootfs			\
       -mlittle-endian               \
       -march=armv5t                 \
@@ -26,21 +27,18 @@ CFLAGS=$(DEFS) $(INCLUDES)          \
       -fno-common                   \
       -fno-omit-frame-pointer       \
       -mapcs                        \
-      -mabi=aapcs-linux
+      -mabi=aapcs-linux \
+      -pg -g
 
 all: clean $(EXEC)
 
-
 $(EXEC): $(OBJS)
-	$(CC) -o $@ $(OBJS) $(LIBS) $(LDFLAGS)
+	$(CC) -o $@ $(OBJS) $(LIBS) $(LDFLAGS) -pg
 
 %.o : %.cpp
-	$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@ 
 
 .PHONY: clean all
 clean:
 	rm -f $(OBJS) $(EXEC) tracking_result.avi *~
 
-send:
-		echo "Sending $(EXEC) to Beagleboard ... "
-		scp ./$(EXEC) root@88.202.161.61:$(BEAGLE_DIR)/.
