7f41a3eb6ca4425419fe9ccd9f5ac8a50b71d3d4
Conme <konpmetaxas@gmail.com>
2017-06-06 19:41:34 +0200

    pdf and CalWeight on DSP without split. SLOW

---
+++ no_split_slow/dsp/task.h
@@ -0,0 +1,44 @@
+#if !defined (TASK_)
+#define TASK_
+
+/*  ----------------------------------- DSP/BIOS Headers            */
+#include <sem.h>
+
+/*  ----------------------------------- Sample Headers              */
+#include <pool_notify_config.h>
+
+#define BLUE               0
+#define GREEN              1
+#define RED                2
+
+
+#define STOP_DSP           0
+#define IDLE               1
+#define LOAD_COLOR         2
+#define RETURN_RESULT      3
+#define EPAN               4
+#define INIT_BLUE          5
+#define INIT_GREEN         6
+#define INIT_RED           7
+#define REPRESENT          8
+#define WEIGHT_BLUE        9
+#define WEIGHT_GREEN      10
+#define WEIGHT_RED        11
+#define WEIGHT_ALL        12
+
+
+typedef struct Task_TransferInfo_tag {
+    Uint16 *        dataBuf ;
+    SEM_Obj         notifySemObj ;
+    Uint32          bufferSize ;
+} Task_TransferInfo ;
+
+Int Task_create (Task_TransferInfo ** transferInfo) ;
+
+Int Task_execute (Task_TransferInfo * transferInfo) ;
+
+Int Task_delete (Task_TransferInfo * transferInfo) ;
+
+
+
+#endif /* !defined (Task_) */
