eeb58f2105d3dc0bc6534ce75a6e534f01475f82
Danielle van der Werff <daniellevanderwerff@gmail.com>
2017-06-06 15:47:22 +0200

    Added timing for neon_pdfweight, and timing+merging of best_no_neon

--- timers/neon_pdfweight_combined_timed/arm_gprof.mk
+++ timers/neon_pdfweight_combined_timed/arm_gprof.mk
@@ -1,23 +1,25 @@
-BASE_TOOLCHAIN=/data/bbToolChain/usr/local/share/codesourcery
+BASE_TOOLCHAIN=/opt/bbToolChain/usr/local/share/codesourcery
 CC=$(BASE_TOOLCHAIN)/bin/arm-none-linux-gnueabi-g++
-BEAGLE_DIR=/home/root/esLAB/danielle/helloDSP
+MCPROF_DIR=~/mcprof
 
-SRCS=meanshift.cpp main.cpp 
+SRCS=meanshift.cpp main.cpp
 OBJS=$(SRCS:%.cpp=%.o)
 EXEC=armMeanshiftExec
+CMD=./$(EXEC) car.avi
 
-LDFLAGS=-lpthread -lm --sysroot=/data/rootfs
+LDFLAGS=-lpthread -lm --sysroot=/opt/rootfs -pg
 LIBS=-lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video \
 	-lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_flann
 
 DEFS=-DARMCC
 INCLUDES=-I. -I$(BASE_TOOLCHAIN)/include
 CFLAGS=$(DEFS) $(INCLUDES)          \
-	  -Wall -O3 -Wfatal-errors 		\
-	  --sysroot=/data/rootfs			\
+	  -Wall -O2 -Wfatal-errors 		\
+	  --sysroot=/opt/rootfs			\
       -mlittle-endian               \
       -march=armv5t                 \
       -mtune=arm9tdmi               \
+      -msoft-float                  \
       -Uarm                         \
       -marm                         \
       -Wno-trigraphs                \
@@ -26,22 +28,18 @@ CFLAGS=$(DEFS) $(INCLUDES)          \
       -fno-omit-frame-pointer       \
       -mapcs                        \
       -mabi=aapcs-linux \
+      -pg -g \
       -mfloat-abi=softfp -mfpu=neon
 
 all: clean $(EXEC)
 
-
 $(EXEC): $(OBJS)
-	$(CC) -o $@ $(OBJS) $(LIBS) $(LDFLAGS)
+	$(CC) -o $@ $(OBJS) $(LIBS) $(LDFLAGS) -pg
 
 %.o : %.cpp
 	$(CC) $(CFLAGS) -c $< -o $@ 
 
-send:
-		echo "Sending $(EXEC) to Beagleboard ... "
-		scp ./$(EXEC) root@88.202.161.61:$(BEAGLE_DIR)/.
-
-
 .PHONY: clean all
 clean:
 	rm -f $(OBJS) $(EXEC) tracking_result.avi *~
+
